24c3ec9ae30adf54228462a41a2376c9ae017ae4
Empressfavs <favouradaavwopiabro@gmail.com>
2024-01-23 09:08:15 -0800

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+class Square: a class Square that defines a square
+"""
+
+
 class Square:
+    """
+    A square class that shows private instance attribute: size
+    """
     def __init__(self, size):
-        self._Square__size = size    
+        self._Square__size = size
