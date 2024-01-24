a3324ef0d062681da93302d8f74f188d4f07daa6
Empressfavs <favouradaavwopiabro@gmail.com>
2024-01-24 02:20:15 -0800

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for lower in str(97, 123):
-        if lower == str:
-
-
+    for upper in str:
+        if ord(upper) >= 97 and ord(upper) <= 122:
+            upper = chr(ord(upper) - 32)
+        print(f"{upper}", end="")
+    print("")
