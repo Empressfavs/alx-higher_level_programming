9d8f3820af96cf1ef76edbd9afc529bd21e87426
Empressfavs <favouradaavwopiabro@gmail.com>
2024-01-24 08:39:54 -0800

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,7 +10,7 @@ class Square:
     """
     def __init__(self, size=0):
         self._square__size = size
-        if type(self._square__size) is not int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         if self._square__size < 0:
             raise ValueError("size must be >= 0")
