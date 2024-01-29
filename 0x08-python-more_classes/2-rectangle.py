618d60a6e45ad05b350d97724de858ed6dac351e
Empressfavs <favouradaavwopiabro@gmail.com>
2024-01-29 06:10:23 -0800

    2-rectangle.py

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+class Rectangle: defines empty class
+"""
+
+
+class Rectangle:
+    """
+    A rectangle class that shows private instance attribute
+    """
+    def __init__(self, width=0, height=0):
+        """
+        __init__: Initializes a Square object
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        return 2 * (self.__width + self.__height)
