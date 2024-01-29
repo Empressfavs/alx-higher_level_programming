c51f4017675e6bad4e7f9de7d9665f3a8d4b2de1
Empressfavs <favouradaavwopiabro@gmail.com>
2024-01-29 05:55:03 -0800

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,8 +12,8 @@ class Rectangle:
         """
         __init__: Initializes a Square object
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
