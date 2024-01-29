46de8ebda2b96f5623be2496a2240055a7a96721
Empressfavs <favouradaavwopiabro@gmail.com>
2024-01-29 02:03:10 -0800

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+class Square: a class Square that defines a square
+"""
+
+
+class Square:
+    """
+    A square class that shows private instance attribute
+    """
+
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        area: calculates the area of the square
+        """
+        return self.__size ** 2
