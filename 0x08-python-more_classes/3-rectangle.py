8ad10168a0b75e4a24bedeea387fef74690b40d6
Empressfavs <favouradaavwopiabro@gmail.com>
2024-01-29 10:03:16 -0800

    3-rectangle.py

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+class Rectangle: defines empty class
+"""
+
+
+class Rectangle:
+    """
+    A rectangle class that shows private instance attribute
+    """
+    def __init__(self, width=0, height=0):
+        """
+        __init__: Initializes a Square object
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        my_perimeter = 2 * (self.width + self.height)
+        if self.width == 0 or self.height == 0:
+            my_perimeter = 0
+        return my_perimeter
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                print("#", end="")
+        return ""
