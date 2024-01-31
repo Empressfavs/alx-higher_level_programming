5d8f0c65a7b88666518746d026cdaa06625fb109
Empressfavs <favouradaavwopiabro@gmail.com>
2024-01-31 13:24:05 +0100

    4-rectangle.py

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -63,6 +63,7 @@ class Rectangle:
             if i != self.__height - 1:
                 print()
         return ""
-
     def __repr__(self):
         return "Rectangle({}, {})".format(self.__width, self.__height)
+
+
