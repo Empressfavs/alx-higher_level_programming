fd1840f481b6974b3da7e0ae2c2135d19828e70b
Empressfavs <favouradaavwopiabro@gmail.com>
2024-02-05 18:39:31 +0100

    a function that returns True if the object is an instance of a class that inherited (directly or indirectly) from the specified class ; otherwise False

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+inherits_from: a function that returns True or False
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    a function that returns True if the object is an instance of a clas
+    that inherited from the specified class
+    """
+    if type(obj) != a_class:
+        return True
+    else:
+        return False
