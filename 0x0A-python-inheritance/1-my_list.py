810d28225e9ed6a7d26c0ae1e705cf86ea2768f6
Empressfavs <favouradaavwopiabro@gmail.com>
2024-02-05 16:30:21 +0100

    a class MyList inherited from list that prints the list in ascending order

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -11,12 +11,3 @@ class MyList(list):
     def print_sorted(self):
         sorted_list = sorted(self)
         print(sorted_list)
-
-
-if __name__ == "__main__":
-    """my_list = MyList()
-    my_list.append(1)
-    my_list.append(4)
-    my_list.append(2)
-    my_list.append(3)
-    my_list.append(5)"""
