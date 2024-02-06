49ff13bf8b93ae7a977681902eca1d0825b0f0d7
Empressfavs <favouradaavwopiabro@gmail.com>
2024-02-06 12:53:58 +0100

    a function that reads a text file (UTF8) and prints it to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+read_file: reads file in the .txt file"
+"""
+
+
+def read_file(filename=""):
+    """
+    a function that reads a text file (UTF8) and prints it to stdout
+    """
+    with open("my_file_0.txt", mode="r", encoding="utf-8") as newfile:
+        print(newfile.read(), end="")
