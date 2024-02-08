accf53f9744ef2276b2df0764fad46db8d43345c
Empressfavs <favouradaavwopiabro@gmail.com>
2024-02-08 10:21:10 +0100

    a function that returns an object (Python data structure) represented by a JSON string

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-import json
 """
 from_json_string: a function that returns the
 JSON representation of an object (string)
 """
+
+
+import json
 
 
 def from_json_string(my_str):
