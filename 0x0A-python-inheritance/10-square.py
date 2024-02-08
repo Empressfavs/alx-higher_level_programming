2bae7d2af1c61648de65fb188fe3c6b64b4d887f
Empressfavs <favouradaavwopiabro@gmail.com>
2024-02-08 03:43:30 +0100

    a class Square that inherits from Rectangle (9-rectangle.py): Instantiation with size: def __init__(self, size)

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Defines a Square class that inherits from BaseGeometry.
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Represents a Square.
+    """
+
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", size)
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
