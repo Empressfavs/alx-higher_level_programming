3ca61184a258e094f174abf17102434772d4deb3
Empressfavs <favouradaavwopiabro@gmail.com>
2024-02-08 10:14:56 +0100

    a function that returns the JSON representation of an object (string)

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-import json
 """
     module that converts an object to json
 """
 
 
+import json
+
+
 def to_json_string(my_obj):
     """
-        to_json_string: converts my_obj to json string
+        converts my_obj to json string
         my_obj: object to convert
     """
 
