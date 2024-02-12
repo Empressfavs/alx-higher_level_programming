0f46229775ad4e92f7f56eba787f2056746a3c36
Empressfavs <favouradaavwopiabro@gmail.com>
2024-02-12 18:24:07 +0100

    Update the class Rectangle by adding validation of all setter methods and instantiation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,13 +11,12 @@ class Rectangle(Base):
     """
     A function that calls the super class with id
     """
-
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
@@ -29,6 +28,7 @@ class Rectangle(Base):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
+        self.__width = value
 
     @property
     def height(self):
@@ -40,6 +40,7 @@ class Rectangle(Base):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
+        self.__height = value
 
     @property
     def x(self):
@@ -51,6 +52,7 @@ class Rectangle(Base):
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
+        self.__x = value
 
     @property
     def y(self):
@@ -62,6 +64,16 @@ class Rectangle(Base):
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
+        self.__y = value
 
     def area(self):
         return self.__width * self.__height
+
+
+"""
+    def display(self):
+        if self.__width == None or self.__height == None:
+            print("")
+        for x in self.__width:
+            for y in self.__height:
+                print("")"""
