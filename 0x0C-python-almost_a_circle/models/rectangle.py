a564e305cf2dabd5471cbbcae20e1ccce6396d37
Empressfavs <favouradaavwopiabro@gmail.com>
2024-02-13 14:07:12 +0100

    Update the class Rectangle by adding the public method def display(self): that prints in stdout the Rectangle instance with the character #

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -89,3 +89,10 @@ class Rectangle(Base):
     def area(self):
         """returns the area of the rectangle"""
         return self.__width * self.__height
+
+    def display(self):
+        """
+        prints in stdout the Rectangle instance with the character #
+        """
+        for i in range(self.height):
+            print("#" * self.width)
